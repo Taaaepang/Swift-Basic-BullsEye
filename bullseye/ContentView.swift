c191c072ba8ba8c9e574cfba7752a2e3e72ced72
Taaapang <ggp02261@gmail.com>
2020-10-23 23:15:19 +0900

    Initial Commit

---
+++ bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  bullseye
+//
+//  Created by 김태광 on 2020/10/23.
+//  Copyright © 2020 김태광. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ bullseyeTests/bullseyeTests.swift
@@ -0,0 +1,34 @@
+//
+//  bullseyeTests.swift
+//  bullseyeTests
+//
+//  Created by 김태광 on 2020/10/23.
+//  Copyright © 2020 김태광. All rights reserved.
+//
+
+import XCTest
+@testable import bullseye
+
+class bullseyeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
